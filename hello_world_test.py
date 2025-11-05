4e7f104278be118768d9a19f019adbd0ea992ce1
Damola <doluniran1@ivytech.edu>
2025-11-05 05:33:01 +0100

    Initial commit: added hello_world_test.py

---
+++ hello_world_test.py
@@ -0,0 +1,8 @@
+print("Hello World")
+cd "C:\Users\RAYTECH\Desktop\Damola\Module1Collaboration"
+git init
+git add .
+git commit -m "Initial commit: added hello_world_test.py"
+git branch -M main
+git remote add origin https://github.com/@Dammyherry/Module-1-Collaboration.git
+git push -u origin main
